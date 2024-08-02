f82d40dd548078f6b1716ff63362a54a6c91ada3
Ubuntu <ubuntu@ip-172-31-16-49.ap-southeast-1.compute.internal>
2024-08-02 14:27:30 +0000

    Change port number

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
 
 app.get('/', (req, res) => {
